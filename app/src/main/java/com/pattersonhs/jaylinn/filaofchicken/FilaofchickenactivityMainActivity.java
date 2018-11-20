ea7f1813ada06a6fca1e17212fa0b177dc64bb22
JayyN <star14.jn@gmail.com>
2018-11-20 09:52:36 -0500

    coded the java part of my ordering app and also made a new activity java

--- app/src/main/java/com/pattersonhs/jaylinn/filaofchicken/FilaofchickenactivityMainActivity.java
+++ app/src/main/java/com/pattersonhs/jaylinn/filaofchicken/FilaofchickenactivityMainActivity.java
@@ -1,5 +1,6 @@
 package com.pattersonhs.jaylinn.filaofchicken;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,9 +16,26 @@ public class FilaofchickenactivityMainActivity extends AppCompatActivity {
     Button Drinks;
     Button Sauces;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.filaofchickenactivity_main);
+
+        Title = (TextView) findViewById(R.id.title);
+        Fries = (Button) findViewById(R.id.Fries);
+        Nuggets = (Button) findViewById(R.id.Nuggets);
+        ChickenSandwhich = (Button) findViewById(R.id.ChickenSandwhich);
+        Drinks = (Button) findViewById(R.id.Drinks);
+        Sauces = (Button) findViewById(R.id.Sauces);
+
+        Fries.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent FriesIntent = new Intent( FilaofchickenactivityMainActivity.this, FriesActivity.class);
+                //startActivity(FriesActivity);
+            }
+        });
+
     }
 }
